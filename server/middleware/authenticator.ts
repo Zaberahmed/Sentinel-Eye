3cf14c9cf157c1f572a13e11ddfb913e838752c7
Sk. Zaber Ahmed <zaber0305@gmail.com>
2023-07-07 10:58:09 +0600

    add: authenticator.ts

---
+++ server/middleware/authenticator.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from 'express';
+const { getSession } = require('./sessionManagement');
+
+const authenticator = async (req: Request, res: Response, next: NextFunction) => {
+	const token = req.headers.authorization?.split(' ')[1];
+
+	if (!token) {
+		return res.status(400).send('Token not found!');
+	}
+
+	const existingSession = getSession(token);
+	if (!existingSession) {
+		return res.status(400).send('Session does not exist!');
+	}
+
+	next();
+};
+
+module.exports = authenticator;
